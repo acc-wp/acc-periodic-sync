acda89f2dccc24613cc5b4f3d94708b701c205fb
francoisbessette <francois.bessette@gmail.com>
2021-03-01 15:39:36 -0500

    Sort logs by date

--- template/acc_logs.php
+++ template/acc_logs.php
@@ -98,34 +98,27 @@
 	<ul class="log_files">
 
 <?php 
-	if ($handle = opendir( $log_directory )) {
-	    while (false !== ($filename = readdir($handle)))
-	    {
-	        if ($filename != "." && $filename != ".." && strtolower(substr($filename, strrpos($filename, '.') + 1)) == 'txt')
-	        {
+	chdir($log_directory);
+	array_multisort(array_map('basename', ($files = glob("*.txt"))), SORT_DESC, $files);
+	foreach($files as $filename)
+	{
+		$split_filename;
+		$page_url = get_site_url() . "/wp-admin/users.php?page=acc_admin_page&log=".$filename;
+		$complete_url = wp_nonce_url( $page_url, 'trash-log', 'acc_nonce' );
+		preg_match('/log_(.*)_(\d+-\d+-\d+)-(\d+-\d+-\d+).txt/mis', $filename, $split_filename);
 
-	        	$split_filename;
-	        	$page_url = get_site_url() . "/wp-admin/users.php?page=acc_admin_page&log=".$filename;
-				$complete_url = wp_nonce_url( $page_url, 'trash-log', 'acc_nonce' );	        	
-	      
-	        	preg_match('/log_(.*)_(\d+-\d+-\d+)-(\d+-\d+-\d+).txt/mis', $filename, $split_filename);
-
-
-	            $log_list .= '
-	            <li>
-	            	<strong>
-	            		Log '. $split_filename[1] .'
-	            	</strong> &mdash; 
-	            	<em> 
-	            	    '. $split_filename[2] .' '. str_replace("-", ":", $split_filename[3]) .'
-	            	</em> &mdash; 
-	            	<a href="'. $log_folder . $filename .'" download>Télécharger</a> | 
-	            	<a href="' . $complete_url .'">Supprimer</a>
-	            </li> ';
-	        }
-	    }
-	    closedir($handle);
-	}
+		$log_list .= '
+		<li>
+			<strong>
+				Log '. $split_filename[1] .'
+			</strong> &mdash;
+			<em>
+				'. $split_filename[2] .' '. str_replace("-", ":", $split_filename[3]) .'
+			</em> &mdash;
+			<a href="'. $log_folder . $filename .'" download>Télécharger</a> | 
+			<a href="' . $complete_url .'">Supprimer</a>
+		</li> ';
+}
 
 	echo $log_list;
 
